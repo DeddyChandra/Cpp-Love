19ed082d851937a8ee5881537559f6369750f693
DeddyChandra <6181801007@student.unpar.ac.id>
2021-11-25 09:20:25 +0700

    cpp love

---
+++ love.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <Windows.h>
+
+using namespace std;
+
+int main(){
+   for(float y = 1.5; y > -1.5; y -= 0.1){
+      for(float x = -1.5; x < 1.5; x += 0.05){
+         float a = x * x + y * y -1;
+         putchar( a * a * a - x * x * y * y * y <= 0.0 ? '*' : ' ');
+      }
+      system("color 0c");
+      putchar('\n');
+   }
+   return 0;
+}
